f6af5b9ffc8da2804e9ca227d6c16ebce41ebe2f
Aryan Irani <64119688+aryanirani123@users.noreply.github.com>
2021-12-08 08:47:05 +0530

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -2,7 +2,7 @@ import gspread
 
 from oauth2client.service_account import ServiceAccountCredentials
 
-from pprint import pprint
+from pprint import pprint as pp
 
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
@@ -16,7 +16,7 @@ sheet = client.open("Students Marks ").sheet1
 #Getting data from the sheet
 data = sheet.get_all_records()
 
-print(data)
+pp(data)
 
 
 #Get the specific row, column and cell from the sheet
@@ -25,7 +25,7 @@ row = sheet.row_values(3)
 col = sheet.col_values(3)
 cell = sheet.cell(1,2).value
 
-pprint(cell)
+pp(cell)
 
 #Inserting data in your sheet 
 
@@ -33,4 +33,4 @@ insertRow = ["Zayn","Malik",12]
 sheet.insert_row(insertRow,7)
 print("The row has been added")
 sheet.delete_row(2)
-print("The row has been deleted")
+pp("The row has been deleted")
